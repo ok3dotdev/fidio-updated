d7998c86067ef5f5a90b772752e4c3b215c4e604
Henry Oke <henryoke@LMG-00187.local>
2023-09-29 14:10:52 -0400

    Stash

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,36 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-<<<<<<< HEAD
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet"></link>
-      </Head>
-      <body className=''>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
-=======
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <script src="https://unpkg.com/@glidejs/glide" crossorigin="anonymous"></script>
-          <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet"></link>
+          <script
+            src='https://unpkg.com/@glidejs/glide'
+            crossorigin='anonymous'
+          ></script>
+          <link
+            href='https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp'
+            rel='stylesheet'
+          ></link>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
->>>>>>> upstream/master
+export default MyDocument;
