7ee0e56efa3ed96513ebf1d3d15bce3650a5a7aa
Jesse <jessealexanderthompson@gmail.com>
2023-11-23 15:51:06 -0500

    Replies Handle Mobile Styling

---
+++ modules/streaming/watch/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "WatchPage", {
+  enumerable: true,
+  get: function get() {
+    return _WatchPage["default"];
+  }
+});
+var _WatchPage = _interopRequireDefault(require("./WatchPage.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
