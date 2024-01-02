ee85934af85265b51ce28efc3455b1d580e1e410
Jesse <jessealexanderthompson@gmail.com>
2024-01-01 23:00:06 -0500

    Fix internal ref

--- modules/internal/index.js
+++ modules/internal/index.js
@@ -3,11 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "internal", {
+Object.defineProperty(exports, "Internal", {
   enumerable: true,
   get: function get() {
     return _Internal["default"];
   }
 });
-var _Internal = _interopRequireDefault(require("./Internal"));
+var _Internal = _interopRequireDefault(require("./internal"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
