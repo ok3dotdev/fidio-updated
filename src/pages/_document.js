03cf1b549b76b2448358386eca1508dd2e16901d
Jesse <jessealexanderthompson@gmail.com>
2024-01-25 01:54:01 -0500

    Initial resolvePageName

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
 					{ PaystackScript }
 				</Head>
 				<body>
-					<div version='0.3.46' business='Tycoon Systems Corp.' style={{ display: 'none' }}></div>
+					<div version='0.3.47' business='Tycoon Systems Corp.' style={{ display: 'none' }}></div>
 					<Main />
 					<NextScript />
 				</body>
