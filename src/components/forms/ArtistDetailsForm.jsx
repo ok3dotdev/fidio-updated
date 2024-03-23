70a978a507150e9e54986a4ba109d8030aa353ff
Henry Oke <henryoke@LMG-00187.local>
2024-03-23 12:17:27 -0400

    Feat: save

--- src/components/forms/ArtistDetailsForm.jsx
+++ src/components/forms/ArtistDetailsForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { useForm, Controller } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 import useSurveyStore from '../../hooks/store';
 
 const ArtistDetailsForm = ({
@@ -12,179 +12,177 @@ const ArtistDetailsForm = ({
   selectedImage,
   lineUpInfo,
   addPerformer,
+  register,
+  control,
 }) => {
-  const { register, handleSubmit, reset, control, setValue } = useForm();
   const { step, setStep } = useSurveyStore();
-  console.log('demeter');
 
   return (
-    <div className='max-w-[500px] mx-auto font-lexend mt-[2rem] mb-[12rem]'>
-      <form onSubmit={handleSubmit(onSubmit)} className='relative'>
-        <div>
-          <div className='flex justify-between items-center'>
-            <h1 className='font-bold text-lg'>Create event</h1>
-            <p>
-              Step {step} <span className='text-dashtext'>of 3</span>
-            </p>
-          </div>
-          <p className='text-dashtext text-sm mt-2'>
-            Don&apos;t worry if you need a break! Your event details are saved
-            as a draft. Come back and complete it anytime.
+    <div>
+      <div>
+        <div className='flex justify-between items-center'>
+          <h1 className='font-bold text-lg'>Create event</h1>
+          <p>
+            Step {step} <span className='text-dashtext'>of 3</span>
           </p>
         </div>
-        <Card className=' dark:bg-transparent mt-8'>
-          <CardTitle className='p-4'>Enter performers</CardTitle>
-          <CardContent className='space-y-4 mt-4'>
-            <div className='space-y-2'>
-              <p className='mb-8'>HEADLINER</p>
-              <label htmlFor='title'>Performer name and photo</label>
-              <div className='flex items-center gap-x-2'>
-                <Input
-                  name='headliner.name'
-                  placeholder='Asake'
-                  className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
-                  ref={register}
-                  {...register('headliner.name')}
-                />
-                <Controller
-                  control={control}
-                  name={'headliner.image'}
-                  render={({ field: { value, onChange, ...field } }) => {
-                    return (
-                      <Input
-                        {...field}
-                        name='headliner'
-                        value={value?.fileName}
-                        placeholder='Upload image'
-                        className='w-36'
-                        onChange={(event) => {
-                          onChange(event.target.files[0]);
-                          handleImageUpload(event);
-                        }}
-                        type='file'
-                        id='picture'
-                      />
-                    );
-                  }}
-                />
-              </div>
-              <div className='space-y-2 mt-4'>
-                <label htmlFor='title'>Bio</label>
-                <div className='flex items-center gap-x-2'>
-                  <Input
-                    name='headliner.bio'
-                    placeholder='Enter bio'
-                    className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
-                    ref={register}
-                    {...register('headliner.bio')}
-                  />
-                </div>
-              </div>
-              {selectedImage && (
-                <div>
-                  <img
-                    src={selectedImage}
-                    alt='Selected Image'
-                    className='mt-4 w-20 h-20 rounded-full object-cover'
-                  />
-                </div>
-              )}
-            </div>
-            <hr className='w-full' />
-            <h3 className='text-dashtext'>OTHER PERFORMERS</h3>
-            <div className='space-y-4'>
-              {lineUpInfo.map((performer, index) => (
-                <div key={index} className='space-y-2'>
-                  <label htmlFor='title'>Performer name and photo</label>
-                  <div className='flex gap-2 mb-2'>
-                    <Input
-                      name={`detailmeta.lineup[${index}].title`}
-                      placeholder='Enter artist, performer or band name'
-                      className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
-                      {...register(`detailmeta.lineup[${index}].title`)}
-                    />
-                    <Controller
-                      control={control}
-                      name={`detailmeta.lineup[${index}].image`}
-                      render={({ field: { onChange, value, ...field } }) => (
-                        <Input
-                          {...field}
-                          value={value?.fileName}
-                          name={`detailmeta.lineup[${index}].image`}
-                          placeholder='Upload image'
-                          accept='image/*'
-                          className='w-36'
-                          onChange={(e) => {
-                            onChange(e.target.files[0]);
-                            handleLineupUpload(e, index);
-                          }}
-                          type='file'
-                          id={`picture-${index}`}
-                        />
-                      )}
-                    />
-                  </div>
-                  {performer.image && (
-                    <div className='flex gap-x-2'>
-                      <img
-                        src={performer.file}
-                        alt='Performer'
-                        className='w-10 h-10 rounded-full object-cover'
-                      />
-                      <Button
-                        className='text-red-500 ml-2'
-                        onClick={() => deletePerformer(index)}
-                        type='button'
-                      >
-                        Delete
-                      </Button>
-                    </div>
-                  )}
-                </div>
-              ))}
-              <div>
-                <Button
-                  type='button'
-                  onClick={addPerformer}
-                  className='w-full dark:bg-dashBorder rounded-[6px] py-1 mt-4 dark:text-white'
-                >
-                  Add another performer{' '}
-                </Button>
-              </div>
-            </div>
-            <hr className='w-full' />
-            <div className='space-y-2'>
-              <label htmlFor='merch'>
-                Sell your merch by sharing a link to your store
-              </label>
+        <p className='text-dashtext text-sm mt-2'>
+          Don&apos;t worry if you need a break! Your event details are saved as
+          a draft. Come back and complete it anytime.
+        </p>
+      </div>
+      <Card className=' dark:bg-transparent mt-8'>
+        <CardTitle className='p-4'>Enter performers</CardTitle>
+        <CardContent className='space-y-4 mt-4'>
+          <div className='space-y-2'>
+            <p className='mb-8'>HEADLINER</p>
+            <label htmlFor='title'>Performer name and photo</label>
+            <div className='flex items-center gap-x-2'>
               <Input
-                name='merch'
-                placeholder='https://'
+                name='headliner.name'
+                placeholder='Asake'
                 className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
-                {...register('merch')}
+                ref={register}
+                {...register('headliner.name')}
+              />
+              <Controller
+                control={control}
+                name={'headliner.image'}
+                render={({ field: { value, onChange, ...field } }) => {
+                  return (
+                    <Input
+                      {...field}
+                      name='headliner'
+                      value={value?.fileName}
+                      placeholder='Upload image'
+                      className='w-36'
+                      onChange={(event) => {
+                        onChange(event.target.files[0]);
+                        handleImageUpload(event);
+                      }}
+                      type='file'
+                      id='picture'
+                    />
+                  );
+                }}
               />
             </div>
-          </CardContent>
-        </Card>
-        <div className='mt-4 space-x-2 absolute right-0'>
-          <Button
-            role=''
-            className='dark:bg-transparent dark:text-white border-1 border-dashBorder dark:hover:bg-transparent'
-            onClick={(e) => {
-              e.preventDefault();
-              setStep(1);
-            }}
-          >
-            Go back safely
-          </Button>
-          <Button
-            type='submit'
-            className='dark:bg-accentY dark:text-white dark:hover:bg-accentY hover:bg-opacity-[0.8] hover:border-accentY focus:border-accentY outline-none shadow-none'
-          >
-            Next
-          </Button>
-        </div>
-      </form>
+            <div className='space-y-2 mt-4'>
+              <label htmlFor='title'>Bio</label>
+              <div className='flex items-center gap-x-2'>
+                <Input
+                  name='headliner.bio'
+                  placeholder='Enter bio'
+                  className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
+                  ref={register}
+                  {...register('headliner.bio')}
+                />
+              </div>
+            </div>
+            {selectedImage && (
+              <div>
+                <img
+                  src={selectedImage}
+                  alt='Selected Image'
+                  className='mt-4 w-20 h-20 rounded-full object-cover'
+                />
+              </div>
+            )}
+          </div>
+          <hr className='w-full' />
+          <h3 className='text-dashtext'>OTHER PERFORMERS</h3>
+          <div className='space-y-4'>
+            {lineUpInfo.map((performer, index) => (
+              <div key={index} className='space-y-2'>
+                <label htmlFor='title'>Performer name and photo</label>
+                <div className='flex gap-2 mb-2'>
+                  <Input
+                    name={`detailmeta.lineup[${index}].title`}
+                    placeholder='Enter artist, performer or band name'
+                    className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
+                    {...register(`detailmeta.lineup[${index}].title`)}
+                  />
+                  <Controller
+                    control={control}
+                    name={`detailmeta.lineup[${index}].image`}
+                    render={({ field: { onChange, value, ...field } }) => (
+                      <Input
+                        {...field}
+                        value={value?.fileName}
+                        name={`detailmeta.lineup[${index}].image`}
+                        placeholder='Upload image'
+                        accept='image/*'
+                        className='w-36'
+                        onChange={(e) => {
+                          onChange(e.target.files[0]);
+                          handleLineupUpload(e, index);
+                        }}
+                        type='file'
+                        id={`picture-${index}`}
+                      />
+                    )}
+                  />
+                </div>
+                {performer.image && (
+                  <div className='flex gap-x-2'>
+                    <img
+                      src={performer.file}
+                      alt='Performer'
+                      className='w-10 h-10 rounded-full object-cover'
+                    />
+                    <Button
+                      className='text-red-500 ml-2'
+                      onClick={() => deletePerformer(index)}
+                      type='button'
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                )}
+              </div>
+            ))}
+            <div>
+              <Button
+                type='button'
+                onClick={addPerformer}
+                className='w-full dark:bg-dashBorder rounded-[6px] py-1 mt-4 dark:text-white'
+              >
+                Add another performer{' '}
+              </Button>
+            </div>
+          </div>
+          <hr className='w-full' />
+          <div className='space-y-2'>
+            <label htmlFor='merch'>
+              Sell your merch by sharing a link to your store
+            </label>
+            <Input
+              name='merch'
+              placeholder='https://'
+              className='bg-dashSides border-[1px] dark:border-dashBorder text-white font-medium'
+              {...register('merch')}
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className='mt-4 space-x-2 absolute right-0'>
+        <Button
+          role=''
+          className='dark:bg-transparent dark:text-white border-1 border-dashBorder dark:hover:bg-transparent'
+          onClick={(e) => {
+            e.preventDefault();
+            setStep(1);
+          }}
+        >
+          Go back safely
+        </Button>
+        <Button
+          type='submit'
+          className='dark:bg-accentY dark:text-white dark:hover:bg-accentY hover:bg-opacity-[0.8] hover:border-accentY focus:border-accentY outline-none shadow-none'
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,24 +1,23 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
-import React from 'react'
-import { AppConfigLayout, PageContainer } from '/modules/internal'
-import { pageDefaults } from '/app.config'
-import { getServerSidePropsDefault } from '/modules/utility.js'
-import { getServerSidePropsFunc } from '/appServer/serverProps'
-import { Menu } from '/modules/menu/'
+import React from 'react';
+import { AppConfigLayout, PageContainer } from '/modules/internal';
+import { pageDefaults } from '/app.config';
+import { getServerSidePropsDefault } from '/modules/utility.js';
+import { getServerSidePropsFunc } from '/appServer/serverProps';
+import { Menu } from '/modules/menu/';
 
-const pageName = 'privacy'
+const pageName = 'Privacy';
 
-export const page = props => {
-	return (
-        <React.Fragment>
-            <PageContainer { ...props } pageName={pageName}>
-                <Menu></Menu>
-                <AppConfigLayout></AppConfigLayout>
-            </PageContainer>
-        </React.Fragment>
-	)
-}
+export const page = (props) => {
+  return (
+    <React.Fragment>
+      <PageContainer {...props} pageName={pageName}>
+        <AppConfigLayout></AppConfigLayout>
+      </PageContainer>
+    </React.Fragment>
+  );
+};
 
 export const getServerSideProps = async (context) => {
   let currentProps = await getServerSidePropsDefault(
@@ -28,4 +27,4 @@ export const getServerSideProps = async (context) => {
   return await getServerSidePropsFunc(currentProps, context);
 };
 
-export default page
+export default page;
