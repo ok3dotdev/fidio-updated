1cbf83dbc369c209e016d0769a887961c438d017
Jesse <jessealexanderthompson@gmail.com>
2024-03-23 23:22:48 -0400

    Google sign in up

--- modules/utility/_app/_app_tools.js
+++ modules/utility/_app/_app_tools.js
@@ -1,34 +1,38 @@
-import{addToCartGlobal,calculateTotal,updateCart,performPurchase}from"../../utility/ecommerce";import{isObjectEmpty}from"/modules/util";const handleSetLoggedIn=(t,e,a)=>{t._loggedIn?a(t._loggedIn):(t=e())&&a(t)},toggleSingleOpenMenu=(t,e,a,o,r)=>{!r&&a&&a.currentMenu&&a.currentMenu==e?o({}):o({currentMenu:e})},handleCheckUserData=async(t,a)=>{if(t&&a){var o={ip:!(!a.meta||isObjectEmpty(a.meta)||"::ffff:127.0.0.1"!=(a.meta.ip&&a.meta.ip)),location:!(!a.meta||isObjectEmpty(a.meta)||a.meta.location)};console.log("Must Check",o);let e=!1;for(let t=0;t<Object.entries(o).length;t++)if(Object.entries(o)[t][1]){e=!0;break}if(e)return o}return!1},handleSetCart=(t,e)=>{var a=JSON.parse(localStorage.getItem("cart"));a?a.user||((a=a).user=t,localStorage.setItem("cart",JSON.stringify(a)),e(a)):t&&(a={user:t,cart:[]},localStorage.setItem("cart",JSON.stringify(a)),e(a))},forceUpdateProps=(t,e)=>{t&&"_cart"===t.dispatch&&e(JSON.parse(window.localStorage.getItem("cart")))},registerSocket=(t,e,a,o,r,l)=>{e||o||(e={reconnectAttempts:1},r.socketpath&&(e.path=r.socketpath,e.port=r.socketPort),a(t(r.socketUrl,e)),o=setTimeout(()=>{l(null)},2e4),l(o))},handleGlobalEvent=async(t,e,a,o)=>{var r,l;t&&("buy"===t.action?(e._setPageError(null),!a&&t.item&&t.style&&t.option&&(r=JSON.parse(localStorage.getItem("cart")),l=await addToCartGlobal(e.apiUrl,e.domainKey,e._loggedIn,r,t.item,{style:t.style,option:t.option},o),e._LocalEventEmitter.dispatch("cart_update",{dispatch:"flashCart"}),l)&&"success"===l.status&&(updateCart(r,l.cart),r=JSON.parse(localStorage.getItem("cart")),l=calculateTotal(r,null,{object:!0}),console.log("snapshot",l),(l=await performPurchase(e.apiUrl,e.domainKey,e._loggedIn,r,o,{snapshot:l}))?"success"===l.status?(l.data&&l.data.cart&&updateCart(r,l.data.cart),l?.data?.order?.id&&(e._LocalEventEmitter.dispatch("cart_update",{dispatch:"purchase",id:l.data.order.id}),console.log("Purchase Success",l))):e._setPageError({message:"Purchase failed",placement:"purchase"}):(console.log(l),e._setPageError({message:"Purchase failed",placement:"purchase"})),o(!1))):"add_to_cart"===t.action?(e._setPageError(null),!a&&t.item&&t.style&&t.option&&(r=JSON.parse(localStorage.getItem("cart")),l=await addToCartGlobal(e.apiUrl,e.domainKey,e._loggedIn,r,t.item,{style:t.style,option:t.option},o),e._LocalEventEmitter.dispatch("cart_update",{dispatch:"flashCart"}),l?"success"===l.status&&updateCart(r,l.cart):e._setPageError({message:"Add to cart failed",placement:"add_to_cart"}))):"add_to_cart_subscribe"===t.action?(e._setPageError(null),!a&&t.item&&t.style&&t.option&&(r=JSON.parse(localStorage.getItem("cart")),l=await addToCartGlobal(e.apiUrl,e.domainKey,e._loggedIn,r,t.item,{style:t.style,option:t.option},o,{subscribe:"monthly"}),e._LocalEventEmitter.dispatch("cart_update",{dispatch:"flashCart"}),l?"success"===l.status&&updateCart(r,l.cart):e._setPageError({message:"Add to cart failed",placement:"add_to_cart"}))):"logout"===t.action&&e._setLoggedIn&&e._setLoggedIn(!1))},registerGoogleSignIn=`// Register google one tap sign in event to pass data to registration/login function
-    const onOneTapSignedInGoogle = data => {
-        let event = new CustomEvent("thirdparty-signin", { "detail": data });
-        document.dispatchEvent(event);
-    }
-    const doGoogleInit = (delay = 250) => {
+import{addToCartGlobal,calculateTotal,updateCart,performPurchase}from"../../utility/ecommerce";import{isObjectEmpty}from"/modules/util";const handleSetLoggedIn=(e,t,a)=>{e._loggedIn?a(e._loggedIn):(e=t())&&a(e)},toggleSingleOpenMenu=(e,t,a,o,r)=>{!r&&a&&a.currentMenu&&a.currentMenu==t?o({}):o({currentMenu:t})},handleCheckUserData=async(e,a)=>{if(e&&a){var o={ip:!(!a.meta||isObjectEmpty(a.meta)||"::ffff:127.0.0.1"!=(a.meta.ip&&a.meta.ip)),location:!(!a.meta||isObjectEmpty(a.meta)||a.meta.location)};console.log("Must Check",o);let t=!1;for(let e=0;e<Object.entries(o).length;e++)if(Object.entries(o)[e][1]){t=!0;break}if(t)return o}return!1},handleSetCart=(e,t)=>{var a=JSON.parse(localStorage.getItem("cart"));a?a.user||((a=a).user=e,localStorage.setItem("cart",JSON.stringify(a)),t(a)):e&&(a={user:e,cart:[]},localStorage.setItem("cart",JSON.stringify(a)),t(a))},forceUpdateProps=(e,t)=>{e&&"_cart"===e.dispatch&&t(JSON.parse(window.localStorage.getItem("cart")))},registerSocket=(e,t,a,o,r,l)=>{t||o||(t={reconnectAttempts:1},r.socketpath&&(t.path=r.socketpath,t.port=r.socketPort),a(e(r.socketUrl,t)),o=setTimeout(()=>{l(null)},2e4),l(o))},handleGlobalEvent=async(e,t,a,o)=>{var r,l;e&&("buy"===e.action?(t._setPageError(null),!a&&e.item&&e.style&&e.option&&(r=JSON.parse(localStorage.getItem("cart")),l=await addToCartGlobal(t.apiUrl,t.domainKey,t._loggedIn,r,e.item,{style:e.style,option:e.option},o),t._LocalEventEmitter.dispatch("cart_update",{dispatch:"flashCart"}),l)&&"success"===l.status&&(updateCart(r,l.cart),r=JSON.parse(localStorage.getItem("cart")),l=calculateTotal(r,null,{object:!0}),console.log("snapshot",l),(l=await performPurchase(t.apiUrl,t.domainKey,t._loggedIn,r,o,{snapshot:l}))?"success"===l.status?(l.data&&l.data.cart&&updateCart(r,l.data.cart),l?.data?.order?.id&&(t._LocalEventEmitter.dispatch("cart_update",{dispatch:"purchase",id:l.data.order.id}),console.log("Purchase Success",l))):t._setPageError({message:"Purchase failed",placement:"purchase"}):(console.log(l),t._setPageError({message:"Purchase failed",placement:"purchase"})),o(!1))):"add_to_cart"===e.action?(t._setPageError(null),!a&&e.item&&e.style&&e.option&&(r=JSON.parse(localStorage.getItem("cart")),l=await addToCartGlobal(t.apiUrl,t.domainKey,t._loggedIn,r,e.item,{style:e.style,option:e.option},o),t._LocalEventEmitter.dispatch("cart_update",{dispatch:"flashCart"}),l?"success"===l.status&&updateCart(r,l.cart):t._setPageError({message:"Add to cart failed",placement:"add_to_cart"}))):"add_to_cart_subscribe"===e.action?(t._setPageError(null),!a&&e.item&&e.style&&e.option&&(r=JSON.parse(localStorage.getItem("cart")),l=await addToCartGlobal(t.apiUrl,t.domainKey,t._loggedIn,r,e.item,{style:e.style,option:e.option},o,{subscribe:"monthly"}),t._LocalEventEmitter.dispatch("cart_update",{dispatch:"flashCart"}),l?"success"===l.status&&updateCart(r,l.cart):t._setPageError({message:"Add to cart failed",placement:"add_to_cart"}))):"logout"===e.action&&t._setLoggedIn&&t._setLoggedIn(!1))},registerGoogleSignIn=e=>`// Register google one tap sign in event to pass data to registration/login function
+        const onOneTapSignedInGoogle = data => {
+            let event = new CustomEvent("thirdparty-signin", { "detail": data });
+            document.dispatchEvent(event);
+        }
+        const doGoogleInit = (delay = 250) => {
+            try {
+                if (!window || (window && !window.googleInitialized)) {
+                    google.accounts.id.initialize({
+                        client_id: '${e}',
+                        callback: onOneTapSignedInGoogle,
+                        use_fedcm_for_prompt: true
+                    })
+                    window.googleInitialized = true
+                    console.log('Google Loaded')
+                }
+                return true
+            } catch (err) {
+                // fail silently
+                setTimeout(() => {
+                    doGoogleInit(delay * 2)
+                }, delay)
+            }
+        }
         try {
-            google.accounts.id.initialize({
-                client_id: '169701902623-9a74mqcbqr38uj87qm8tm3190cicaa7m.apps.googleusercontent.com',
-                callback: onOneTapSignedInGoogle
-            })
-            console.log('Google Loaded')
-            return true
+            let createdScripts = document.getElementsByClassName("lazyOnload");
+            if (createdScripts && createdScripts.length > 1) { // Remove duplicate scripts
+                for (let i = 1; i < createdScripts.length; i++) {
+                    createdScripts[i].remove();
+                }
+            }
+            doGoogleInit()
         } catch (err) {
             // fail silently
             setTimeout(() => {
-                doGoogleInit(delay * 2)
-            }, delay)
-        }
-    }
-    try {
-        let createdScripts = document.getElementsByClassName("lazyOnload");
-        if (createdScripts && createdScripts.length > 1) { // Remove duplicate scripts
-            for (let i = 1; i < createdScripts.length; i++) {
-                createdScripts[i].remove();
-            }
-        }
-        doGoogleInit()
-    } catch (err) {
-        // fail silently
-        setTimeout(() => {
-            doGoogleInit(500)
-        }, 250)
-    }`;export{handleSetLoggedIn,toggleSingleOpenMenu,handleCheckUserData,handleSetCart,forceUpdateProps,registerSocket,handleGlobalEvent,registerGoogleSignIn};
+                doGoogleInit(500)
+            }, 250)
+        }`;export{handleSetLoggedIn,toggleSingleOpenMenu,handleCheckUserData,handleSetCart,forceUpdateProps,registerSocket,handleGlobalEvent,registerGoogleSignIn};
