444533cdf5e61765713b35ddfd63ea01a196b12f
Jesse <jessealexanderthompson@gmail.com>
2024-03-24 01:26:20 -0400

    General

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
 					{ PaystackScript }
 				</Head>
 				<body>
-					<div version='0.5.17' business='Tycoon Systems Corp.' style={{ display: 'none' }}></div>
+					<div version='0.5.28' business='Tycoon Systems Corp.' style={{ display: 'none' }}></div>
 					<Main />
 					<NextScript />
 				</body>
