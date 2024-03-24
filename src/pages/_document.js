71d3eff23dc9f1c01b0a3f1261ee28710ab720aa
Jesse <jessealexanderthompson@gmail.com>
2024-03-24 09:49:10 -0400

    Doc

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { SliderStyles, SliderTheme, GoogleFontsLink, PaystackScript } from '/modules/internal/localImports'
+import Document, { Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head>
-					{ SliderStyles }
-					{ SliderTheme }
-					{ GoogleFontsLink }
-					{ PaystackScript }
-				</Head>
 				<body>
 					<Main />
 					<NextScript />
