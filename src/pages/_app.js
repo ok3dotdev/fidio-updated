e20c7b053b3874098e17be7f8c881cf2c3ca83f3
Jesse <jessealexanderthompson@gmail.com>
2024-03-23 23:36:30 -0400

    _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,19 +4,14 @@ import React from 'react'
 import '../styles/globals.scss' // Place style import declarations in /styles/styles.scss and actual style css files under /styles/appstyles/
 import Head from 'next/head'
 import { Internal } from '/modules/internal/'
-import { GoogleGsiClient, GoogleSignInRegister } from '/modules/internal/localImports'
+
 
 function MyApp({ Component, pageProps }) {
   	return (
 		<div>
 			<Head>
-				<meta name="google-signin-client_id" content="169701902623-9a74mqcbqr38uj87qm8tm3190cicaa7m.apps.googleusercontent.com"/>
 				<title>{pageProps.siteTitle}</title>
 			</Head>
-			<>
-				{ GoogleGsiClient }
-				{ GoogleSignInRegister }
-			</>
     		<Internal {...pageProps} _MasterPageComponent={Component} /> {/* Application Start */}
 		</div>
   	)
