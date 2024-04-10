720bd1de7614fc241627f87df4737e4054bad338
Jesse <jessealexanderthompson@gmail.com>
2024-04-09 20:44:06 -0400

    Update server props backup

--- appServer/defaults/serverProps_backup.js
+++ appServer/defaults/serverProps_backup.js
@@ -14,12 +14,19 @@ const getServerSidePropsFunc = async (data, context) => {
     const variables = resolveVariables()
     const config = resolveConfig(variables)
     const resolvedPage = resolvePage(config, context.req.url)
-    // You can see the path path in the url at resolvedPage.url. For example if you want to fetch meta data or default page load props for /p you can run a conditional below
-    // if (resolvedPage && resolvedPage.url === '/p') { // Match for specific page
-    // ... Fetch shopProfile and event data for p page
-    // data = await getPropDefaults(data, context, [ 'profile', 'event', 'watch', 'product', 'shopProfile' ], { u: 'username' }) // This runs without any access to state so you must use query params either as an argument as shown here or in the url e.g http://localhost:3000/privacy?u=theusername
+    // You can see the path path in the url at resolvedPage.url to match requests based on url user is on
+    // if (resolvedPage && resolvedPage.url === '/shopPage') { // Match for specific page
+
+    // You can make requests for prop defaults from server for CDN or user info, shop info, etc
+    // const tempData = await getPropDefaults(data, context, [ 'profile', 'product', 'shopProfile' ], { u: 'username' }) // This runs without any access to state so you must use query params either as an argument as shown here or in the url e.g http://localhost:3000/privacy?u=theusername
+    // data.props = Object.assign(data.props, tempData) // Merge data
+
+    // You can just run call and it will attempt to overwrite matching data on props
+    // await getPropDefaults(data, context, [ 'profile', 'event', 'watch', 'product', 'shopProfile' ], { u: 'username' })
+
     // ... You can also add your own data to props by setting it on data object
-    // data['my_data'] = myData
+    // data.props['my_data'] = myData
+
     // }
 
     // Now your data will appear in props on any component!
