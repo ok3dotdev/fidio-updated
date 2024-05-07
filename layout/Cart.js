4f059c7f5619b95604a2933b80f06bb1f6448954
Jesse <jessealexanderthompson@gmail.com>
2024-05-07 18:47:07 -0400

    Condiitonal checkout

--- layout/Cart.js
+++ layout/Cart.js
@@ -10,66 +10,52 @@ import {
   PurchaseButton,
 } from '../modules/ecommerce/cart';
 import Close from '@mui/icons-material/Close';
-import { fireGlobalEvent } from '/modules/utility/utility';
-import apiReq from '/modules/utility/api/apiReq'; // Import API for making DB Requests
+import { fireGlobalEvent } from '/modules/utility/utility'
+import apiReq from '/modules/utility/api/apiReq' // Import API for making DB Requests
 import { SignIn } from '../modules/onboarding/signin';
-import { Input } from '@/components/ui/input';
 
 const Module = (props) => {
   const close = React.useCallback((e) => {
     props._toggleSingleOpenMenu(e, 'cart');
-    props.setCartOpen(false);
   });
 
-  // if (!loading && ticket) {
-  //   console.log('values', props.open);
-  // }
-
   // Use refs to easily track value inputs
-  const firstName = React.useRef();
-  const lastName = React.useRef();
-  const email = React.useRef();
+  const firstName = React.useRef()
+  const lastName = React.useRef()
+  const email = React.useRef()
 
-  const intendsToSignInAsGuest =
-    !props?._loggedIn &&
-    (email?.current?.value ||
-      firstName?.current?.value ||
-      lastName?.current?.value); // Guess intent based on input email
+  const intendsToSignInAsGuest = !props?._loggedIn && email?.current?.value // Guess intent based on input email
 
   // Attempt anonymous sign in with input info here. You can run manually or pass as f to PurchaseButton
   const doFunc = async () => {
-    props.setPageError(null);
-    console.log('jmmm')
-    if (!intendsToSignInAsGuest && !props._loggedIn) {
-      // If we detect no input and user clicks purchase give next action
-      props.setPageError({
-        message: 'Add email to sign in Anonymously or use one-click sign in',
-        placement: 'purchase',
-      });
-      return false;
+    props.setPageError(null)
+    if (props?._loggedIn?.meta?.account === 'full') {
+      return true
+    } else if (!intendsToSignInAsGuest && !props._loggedIn) { // If we detect no input and user clicks purchase give next action
+        props.setPageError({
+            message: 'Add email to sign in Anonymously or use one-click sign in',
+            placement: 'purchase'
+        })
+        return false
     }
-    await apiReq('/onboarding/signinunregistered', {
-      // If no logged in user this will attempt anonymous sign on. Can run this even if signed in it will not fire
-      email: email?.current?.value,
-      props: props,
-      firstName: firstName?.current?.value, // Optional
-      lastName: lastName?.current?.value, // Optional
-    });
-  };
+    await apiReq('/onboarding/signinunregistered', { // If no logged in user this will attempt anonymous sign on. Can run this even if signed in it will not fire
+            email: email?.current?.value,
+            props: props,
+            firstName: firstName?.current?.value, // Optional
+            lastName: lastName?.current?.value // Optional
+        })
+    }
 
-  // This ensures that we will attempt checkout after save credit card
-  const saveBillingInfoFunction = React.useCallback((e) => {
-    fireGlobalEvent(
-      {
-        event: 'checkout',
-      },
-      props._LocalEventEmitter
-    );
-  });
+// This ensures that we will attempt checkout after save credit card
+const saveBillingInfoFunction = React.useCallback(e => {
+    fireGlobalEvent({
+        event: 'checkout'
+    }, props._LocalEventEmitter)
+})
 
   return (
     <div
-      className={`Ecommerce_Cart_Container p-8 ${props.className} ${
+      className={`Ecommerce_Cart_Container ${props.className} ${
         props.open || (props?.menuOpen && !props?.closing)
           ? 'Ecommerce_Cart_Container_Visible'
           : ''
@@ -89,70 +75,44 @@ const Module = (props) => {
               props?.fetchBusy ? 'fetchNotBusy fetchBusy' : 'fetchNotBusy'
             }`}
           ></div>
-          <input
-            id='Ecommerce_CreditCard_FullNameInput'
-            type='text'
-            style={{ display: 'none' }}
-            devnote='Credit card name input'
-            value={
-              props?._loggedIn?.meta?.firstName
-                ? `${props._loggedIn.meta.firstName}${
-                    props?._loggedIn?.meta?.lastName
-                      ? ` ${props?._loggedIn?.meta?.lastName}`
-                      : ''
-                  }`
-                : firstName?.current?.value
-                ? `${firstName.current.value}${
-                    lastName?.current?.value ? ` ${lastName.current.value}` : ''
-                  }`
-                : null
-            }
-          />
+          <input id='Ecommerce_CreditCard_FullNameInput' type='text' style={{ display: 'none' }} devnote='Credit card name input' value={props?._loggedIn?.meta?.firstName ? `${props._loggedIn.meta.firstName}${props?._loggedIn?.meta?.lastName ? ` ${props?._loggedIn?.meta?.lastName}` : ''}` : firstName?.current?.value ? `${firstName.current.value}${lastName?.current?.value ? ` ${lastName.current.value}` : ''}` : null} />
           <div className='Ecommerce_Cart_Internal_Container'>
-            <div className='flex justify-between mb-5'>
-              <h5 className={'Ecommerce_Label font-bold text-2xl'}>Checkout</h5>
-              <div className='cursor-pointer rounded-full bg-dashSides flex items-center justify-center w-8 h-8'>
-                <Close className='mx-auto my-auto' onClick={close}></Close>{' '}
-                {/* Use your close icon here */}
+            <div className='flex justify-between'>
+              <h5 className={'Ecommerce_Label'}>Checkout</h5>
+              <div style={{ margin: '.125rem', cursor: 'pointer' }}>
+                <Close onClick={close}></Close> {/* Use your close icon here */}
               </div>
             </div>
             <div className='flex Ecommerce_Layout_Data_Container'>
               <div>
                 <div className='Ecommerce_Layout_Data_Details'>
-                  <p className='mb-8 font-semibold'>Billing Information</p>
-                  <div className='mb-4 w-full'>
-                    <label className='flex mb-2'>First Name</label>
-                    <Input
-                      type='text'
-                      placeholder='First Name'
-                      className='w-full p-2 py-3 rounded-[6px] dark:border-neutral600 border-[1px] bg-dashSides'
-                      ref={firstName}
-                    />
+                  <p className='mb-2 font-semibold'>Billing Information</p>
+                  <div className='mb-2'>
+                    <label className='flex'>First Name</label>
+                    <input type='text' placeholder='First Name' ref={firstName}></input>
                   </div>
-                  <div className='mb-4 '>
-                    <label className='flex mb-2'>Last Name</label>
-                    <Input
-                      type='text'
-                      placeholder='Last Name'
-                      className='w-full p-2 py-3 rounded-[6px] dark:border-neutral600 border-[1px] bg-dashSides'
-                      ref={lastName}
-                    />
+                  <div className='mb-2'>
+                    <label className='flex'>Last Name</label>
+                    <input type='text' placeholder='Last Name' ref={lastName}></input>
                   </div>
-                  <div className='mb-4'>
-                    <label className='flex mb-2'>Email</label>
-                    <Input
-                      type='text'
-                      placeholder='Email'
-                      className='w-full p-2 py-3 rounded-[6px] dark:border-neutral600 border-[1px] bg-dashSides'
-                      ref={email}
-                    />
+                  <div className='mb-2'>
+                    <label className='flex'>Email</label>
+                    <input type='text' placeholder='Email' ref={email}></input>
                   </div>
-                  {!props?._loggedIn ? <SignIn {...props} /> : null}
                   <div
-                    className='flex gap-p5 items-center'
-                    style={{ marginTop: '1rem' }}
-                  >
-                    <input type='checkbox' className='w-4 h-4 border-white' />
+                    style={{
+                        borderBottom: '2px dashed grey',
+                        marginTop: '1rem',
+                        marginBottom: '1rem',
+                    }}
+                    ></div>
+                    {
+                        !props?._loggedIn
+                            ? <SignIn { ...props } />
+                            : null
+                    }
+                  <div className='flex gap-p5' style={{ marginTop: '1rem' }}>
+                    <input type='checkbox' />
                     <div
                       className='text-dashtext'
                       style={{ fontSize: '.74rem' }}
@@ -165,29 +125,23 @@ const Module = (props) => {
                 </div>
                 <div
                   className='Ecommerce_Layout_CreditCardContainer'
-                  style={{ marginTop: '2rem' }}
+                  style={{ marginTop: '1rem' }}
                 >
-                  <p className='mb-8 font-semibold'>Pay With</p>
+                  <p className='mb-2 font-semibold'>Pay With</p>
                   <div>
                     <label className='mb-2'>Credit/Debit card</label>
                   </div>
-                  <CartCc
-                    {...props} expressCheckout={true}
-                  />
-                  <PurchaseButton
-                    {...props}
-                    f={doFunc}
-                    text={intendsToSignInAsGuest ? 'Purchase as Guest' : null}
-                  />
+                  <CartCc {...props} saveBillingInfoText={'Purchase'} saveBillingInfoFunction={saveBillingInfoFunction} />
+                  <PurchaseButton {...props} f={doFunc} text={intendsToSignInAsGuest ? 'Purchase as Guest' : null} />
                   <div className='text-dashtext' style={{ fontSize: '.74rem' }}>
                     We use industry-standard security measure to protect your
                     information.
                   </div>
                 </div>
               </div>
-              <div className='Ecommerce_Layout_OrderSummary h-auto self-start'>
+              <div className='Ecommerce_Layout_OrderSummary'>
                 <div>
-                  <p className='mb-8 font-semibold'>Order Summary</p>
+                  <p className='mb-2 font-semibold'>Order Summary</p>
                   <CartItems {...props} />
                 </div>
                 <div
