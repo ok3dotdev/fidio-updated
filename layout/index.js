00d6af805902666ebdfbb1520ab6642e13f39ded
Henry Oke <henryoke@LMG-00187.local>
2024-06-12 09:49:23 -0400

    Feat: blog deploy jun 12

--- layout/index.js
+++ layout/index.js
@@ -1,15 +1,19 @@
 import Cart from './Cart'
 import Watch from './Watch'
+import Order from './Order'
 import Profile from './Profile'
 import Article from './Article'
 import DropMenu from './DropMenu'
 import Menu from './Menu'
+import Help from './Help'
 
 export {
     Article,
     Cart,
+    Order,
     Profile,
     Watch,
     DropMenu,
-    Menu
+    Menu,
+    Help
 }
