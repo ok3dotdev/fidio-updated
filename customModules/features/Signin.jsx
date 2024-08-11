f8e8df4a894f382772b8bea4d08a4e6e55509451
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-08-11 12:53:29 -0400

    implement styling to the SignIn container

--- customModules/features/Signin.jsx
+++ customModules/features/Signin.jsx
@@ -1,39 +1,34 @@
-import React from 'react';
-import { SignIn, Username } from '../../modules/onboarding/signin';
+import React from "react";
+import { SignIn, Username } from "../../modules/onboarding/signin";
 
 const Signin = (props) => {
   //   // ('signin', props);
   return (
-    <div>
-      <div className='w-full h-[100vh] fixed'>
-        <div className='max-w-3xl mx-auto h-full w-full lg:grid grid-cols-1 gap-5 lg:gap-8 lg:grid-cols-2 flex flex-col justify-start lg:justify-center'>
-          <div className='lg:flex justify-start lg:justify-center items-center col-span-1 mt-[10%] hidden'>
-            <img
-              src='/img/internal/frame2.png'
-              alt=''
-              width={'450px'}
-              className='w-[300px] lg:w-[450px]'
-            />
-          </div>
-          <div className='flex flex-col gap-y-2 justify-start lg:justify-center items-center lg:mt-0'>
-            <div className='w-full lg:hidden block'>
-              <img
-                src='/img/internal/frame2.png'
-                alt=''
-                width={'250px'}
-                className='w-[100px] lg:w-[250px]'
-              />
-            </div>
-            <p className='text-2xl mb-8 font-bold'>Log In or Sign Up</p>
-            <Username
-              redirectOnAuth={'/browse'}
-              {...props}
-              prompt={'Username'}
-              confirm={'Continue'}
-            />
-            <SignIn redirectOnAuth={'/browse'} {...props} />
-          </div>
+    <div className="font-lexend bg-dashBg">
+      <div className="w-full h-[100vh] fixed flex justify-center flex-col items-center bg-dashBg">
+        <p className="text-lg mb-8 font-bold">Log In or Sign Up</p>
+        <div className="p-2 w-2/6 max-w-[400px] min-w-[250px] max-h-[508px] bg-dashSides border border-opacity-5 border-white rounded ">
+          <p className="text-left p-2 ">
+            Sign in to access your tickets, viewed events and much more
+          </p>
+
+          <Username
+            redirectOnAuth={"/browse"}
+            {...props}
+            prompt={"Username"}
+            confirm={"Continue"}
+          />
+
+          {/* <p className="text-dashtext m-4">Or</p> */}
+
+          <SignIn redirectOnAuth={"/browse"} {...props} />
         </div>
+        <p className="p-4">
+          New here?{" "}
+          <a href="">
+            <span className="text-accentY">Sign up instead</span>
+          </a>
+        </p>
       </div>
     </div>
   );
