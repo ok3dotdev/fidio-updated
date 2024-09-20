81908b7683520817a2fe0262d91e21620625cc83
Henry Oke <henryoke@LMG-00187.phub.net.cable.rogers.com>
2024-09-19 21:52:26 -0400

    Feat: fix vault ribbons and list pages

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 // You must set this line to your own website for PRODUCTION builds
 
 // const website = 'http://localhost:3020'; // You must set this line to your own website for PRODUCTION builds
-const website = 'https://development.fidio.ca';
-// const website = 'https://www.fidio.ca';
+// const website = 'https://development.fidio.ca';
+const website = 'https://www.fidio.ca';
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -13,7 +13,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // assetPrefix: website,
+  assetPrefix: website,
   reactStrictMode: false, // Helps with debugging, but should be off to ensure runtime does not fire twice
   images: {
     domains: [
